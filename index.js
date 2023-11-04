9a4a03d33ccc81d0956d93aee7d380e771ae5213
Veronika Postnikova <heyverkim@gmail.com>
2023-11-05 01:14:13 +0600

    add some changes

--- index.js
+++ index.js
@@ -15,7 +15,17 @@ function calculateDate() {
         return word[(diffDays % 100 > 4 && diffDays % 100 < 20) ? 2 : cases[(diffDays % 10 < 5) ? diffDays % 10 : 5]];
     }
     result.textContent = `До вашего дня рождения осталось ${diffDays} ${wordForm(diffDays, ['день', 'дня', 'дней'])} `;
-}
+} /*взяла значение из инпута, преобразовала в unix и рассчитала остаток дней. далее добавила функцию которая проверят и выводит правильное склонение слова "день" в зависимости от значения diffDays */
+
+button.addEventListener('click', function () {
+    if (inputDate.value == "") {
+        result.textContent = "Пожалуйста, введите дату рождения";
+        result.classList.toggle('alert');
+        return false;
+    } else {
+        calculateDate();
+        result.classList.toggle('alert');
+    }
+}); /* добавила функцию проверяющую выбор даты пользователем, если дата не выбрана, то выйдет красная надпись с просьбой выбрать. иначе вызовет функцию рассчета кол-ва дней до дня рождения  */
 
 
-button.addEventListener('click', calculateDate);
